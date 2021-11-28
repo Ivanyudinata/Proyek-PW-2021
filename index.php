f0b20199f2849cb876a2676eb4f4f8e10112734b
Ivanyudinata <65076612+Ivanyudinata@users.noreply.github.com>
2021-11-28 17:42:07 +0700

    Tampilan Awal

---
+++ index.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Skibble</title>
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@100;200;300;500;600&display=swap" rel="stylesheet">
+
+    <!-- Fontawesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <style>
+        body, html {
+            width: 100%;
+            height: 100%;
+        }
+
+        * {
+            font-family: 'Montserrat Alternates', sans-serif;
+        }
+    </style>
+
+</head>
+<body class="bg-light">
+
+    <div class="container-fluid m-0 p-0">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light bg-transparent text-dark w-100" style="z-index: 999">
+                <div class="container-fluid w-100">
+                    <div class="row w-100">
+                        <div class="col-2 d-flex align-items-center text-dark">
+                            <a class="navbar-brand" href="#" style="font-weight: 400 !important">Skibble</a>
+                        </div>
+                        <div class="col-8 d-flex align-items-end justify-content-center">
+                            <ul class="list-inline fw-light">
+                                <li class="list-inline-item mx-2">
+                                    <a href="" class="text-decoration-none text-dark">pria</a>
+                                </li>
+                                <li class="list-inline-item mx-2">
+                                    <a href="" class="text-decoration-none text-dark">wanita</a>
+                                </li>
+                                <li class="list-inline-item mx-2">
+                                    <a href="" class="text-decoration-none text-dark">anak</a>
+                                </li>
+                                <li class="list-inline-item mx-2">
+                                    <a href="" class="text-decoration-none text-dark">sports</a>
+                                </li>
+                                <li class="list-inline-item mx-2">
+                                    <a href="" class="text-decoration-none text-dark">brand</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-2 fw-light">
+                            <div class="flex flex-column w-100 justify-content-end">
+                                <div class="w-100 text-end">
+                                    <ul class="list-inline p-0 m-0 fw-light">
+                                        <li class="list-inline-item">
+                                            <a href="" class="text-decoration-none text-dark" style="font-size: 12px;">Bantuan</a>
+                                        </li>
+                                        <li class="list-inline-item">
+                                            <a href="" class="text-decoration-none text-dark" style="font-size: 12px;">masuk</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="mt-2 d-flex justify-content-center">
+                                    <i class="fas fa-shopping-bag fs-2"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+
+        <div class="container-fluid bg-secondary w-100 mt-3" style="height: 400px;">
+
+        </div>
+
+        <div class="hstack gap-5 w-100 mx-auto justify-content-center my-4 py-2 px-5">
+            <div class="p-3 border border-1 rounded row">
+                <dt class="col-3 d-flex align-items-center justify-content-center">
+                    <i class="fas fa-truck fs-4"></i>
+                </dt>
+                <dd class="col-9 p-0 m-0 ps-2 fw-light">Free Shipping</dd>
+            </div>
+            <div class="p-3 border border-1 rounded row">
+                <dt class="col-3 d-flex align-items-center justify-content-center">
+                    <i class="fas fa-money-check fs-4"></i>
+                </dt>
+                <dd class="col-9 p-0 m-0 ps-2 fw-light">Secure Payment</dd>
+            </div>
+            <div class="p-3 border border-1 rounded row">
+                <dt class="col-3 d-flex align-items-center justify-content-center">
+                    <i class="fas fa-headphones fs-4"></i>
+                </dt>
+                <dd class="col-9 p-0 m-0 ps-2 fw-light">24/7 Support</dd>
+            </div>
+        </div>
+
+        <div class="container text-center mt-5">
+            <h4>Our Product</h4>
+            <div class="hstack w-100 gap-3 text-center d-flex justify-content-center my-4">
+                <span class="">All</span>
+                <span class="text-secondary">Men</span>
+                <span class="text-secondary">Women</span>
+            </div>
+            <div class="card-group gap-5 mt-4">
+                <div class="card border">
+                    <img src="assets/bg.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title m-0 p-0">Jacket skinny fit grey for men</h5>
+                        <small class="text-secondary">4.9</small>
+                        <p class="card-text mt-2">
+                            <sup>Rp</sup> 79.000
+                        </p>
+                        <button class="btn btn-outline-dark w-100" style="border-radius: 0 !important;">buy now</button>
+                    </div>  
+                </div>
+                <div class="card">
+                    <img src="assets/bg.jpg" class="card-img-top" alt="...">
+                    <div class="card-body border">
+                        <h5 class="card-title m-0 p-0">Jacket skinny fit grey for men</h5>
+                        <small class="text-secondary">4.9</small>
+                        <p class="card-text mt-2">
+                            <sup>Rp</sup> 79.000
+                        </p>
+                        <button class="btn btn-outline-dark w-100" style="border-radius: 0 !important;">buy now</button>
+                    </div>  
+                </div>
+                <div class="card">
+                    <img src="assets/bg.jpg" class="card-img-top" alt="...">
+                    <div class="card-body border">
+                        <h5 class="card-title m-0 p-0">Jacket skinny fit grey for men</h5>
+                        <small class="text-secondary">4.9</small>
+                        <p class="card-text mt-2">
+                            <sup>Rp</sup> 79.000
+                        </p>
+                        <button class="btn btn-outline-dark w-100" style="border-radius: 0 !important;">buy now</button>
+                    </div>  
+                </div>
+                <div class="card">
+                    <img src="assets/bg.jpg" class="card-img-top" alt="...">
+                    <div class="card-body border">
+                        <h5 class="card-title m-0 p-0">Jacket skinny fit grey for men</h5>
+                        <small class="text-secondary">4.9</small>
+                        <p class="card-text mt-2">
+                            <sup>Rp</sup> 79.000
+                        </p>
+                        <button class="btn btn-outline-dark w-100" style="border-radius: 0 !important;">buy now</button>
+                    </div>  
+                </div>
+            </div>
+        </div>
+
+        <footer class="text-center py-3 mt-5">
+            Skibble
+        </footer>
+    
+
+</body>
+</html>
